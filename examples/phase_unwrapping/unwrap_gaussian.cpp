260f211575ded359d9fd839ca205261e97174c31
Matt Gara <gara.matt@gmail.com>
2019-09-04 21:15:23 -0700

    Implemented working version of SlimCuts using undirected graphs. Testing showing working version is under examples/phase_unwrapping/ unwrap_gaussian.cpp.

--- examples/phase_unwrapping/unwrap_gaussian.cpp
+++ examples/phase_unwrapping/unwrap_gaussian.cpp
@@ -34,7 +34,11 @@
 #include "maxflow_bk.h"
 #include "maxflow_hpf.h"
 #include "maxflow_ibfs.h"
+#include "maxflow_undirected.h"
+#include "maxflow_undirected_slimcuts.h"
 #include "util/timer.h"
+
+//#define USE_DIRECTED
 
 template <typename vector> void create_unwrapping_grid(int size, vector &arc) {
   for (int y = 0; y < size; ++y) {
@@ -62,7 +66,7 @@ void create_unwrapping_problem(const real_vector &wr, const int_vector &arcs,
     int s = arcs[2 * iarc + 0];
     int t = arcs[2 * iarc + 1];
     double dphi = wr[t] - wr[s];
-    double est = std::arg(std::polar(1.,dphi));
+    double est = std::arg(std::polar(1., dphi));
     int ambig = -std::round((dphi - est) / (2 * M_PI));
     ambigs.push_back(ambig);
   }
@@ -75,14 +79,17 @@ void solve_unwrapping_problem(int npt, const int_vector &arcs,
 
   size_t narc = arcs.size() / 2;
 
-
   int iter = 0;
-  const int max_iter = 100;
-  while ( iter++ < max_iter ) {
+  const int max_iter = 200;
+  while (iter++ < max_iter) {
     util::Timer timer_setup, timer_maxflow;
     timer_setup.tic();
+#ifdef USE_DIRECTED
     Graph graph(npt, narc);
-    int_vector tweights(npt,0);
+#else
+    Graph graph(npt);
+#endif
+    int_vector tweights(npt, 0);
     for (size_t iarc = 0; iarc < narc; ++iarc) {
       int s = arcs[2 * iarc + 0];
       int t = arcs[2 * iarc + 1];
@@ -93,33 +100,40 @@ void solve_unwrapping_problem(int npt, const int_vector &arcs,
         shifted_amb = -shifted_amb;
       }
       if (shifted_amb == 0) {
-        graph.add_arc(s, t, 1, 1);
+        int wgt = 100 + (std::rand() % 10000); // random
+//        int wgt = 1; // random
+#ifdef USE_DIRECTED
+        graph.add_arc(s, t, wgt, wgt );
+#else
+        graph.add_arc(s, t, wgt);
+#endif
       } else if (shifted_amb > 0) {
         tweights[s] += 1;
         tweights[t] -= 1;
       }
     }
     for (int i = 0; i < npt; ++i) {
-        int tw = tweights[i];
-        if ( std::abs(tw) > 1 )
-        fprintf(stderr,"tw=%d\n",tw);
-        if ( tw > 0 ) {
-          graph.set_tweights(i,tw,0);
+      int tw = tweights[i];
+      if (std::abs(tw) > 1)
+        //        fprintf(stderr,"tw=%d\n",tw);
+        if (tw > 0) {
+          graph.set_tweights(i, tw, 0);
         } else if (tw < 0) {
-          graph.set_tweights(i,0,-tw);
+          graph.set_tweights(i, 0, -tw);
         }
     }
+    //    graph.test_contraction();
     timer_setup.toc();
     timer_maxflow.tic();
     long mf = graph.maxflow();
     timer_maxflow.toc();
-    fprintf(stderr,"mf=%ld\n",mf);
-    fprintf(stderr,"setup time: %f, maxflow timer: %f\n",timer_setup.elapsed_seconds(),
-            timer_maxflow.elapsed_seconds() );
+    fprintf(stderr, "mf=%ld\n", mf);
+    fprintf(stderr, "setup time: %f, maxflow timer: %f\n",
+            timer_setup.elapsed_seconds(), timer_maxflow.elapsed_seconds());
     bool something_source = false;
     bool something_sink = false;
     for (int i = 0; i < npt; ++i) {
-      if (graph.what_segment(i)==1) {
+      if (graph.what_segment(i) == 1) {
         x[i] += 1;
         something_sink = true;
       } else {
@@ -161,14 +175,22 @@ int main(int argc, char *argv[]) {
 
   create_unwrapping_grid(size, arcs);
   create_unwrapping_problem(gauss, arcs, ambigs);
-  solve_unwrapping_problem<int_vector,maxflowlib::GraphBK<int,int,int,int> >(npt, arcs, ambigs, x);
+
+#ifdef USE_DIRECTED
+    typedef  maxflowlib::GraphBK<int,int,int,int> GraphType;
+#else
+  typedef maxflowlib::UndirectedGraphSlimCuts<
+      maxflowlib::GraphBK<int, int, int, int>>
+      GraphType;
+#endif
+
+  solve_unwrapping_problem<int_vector, GraphType>(npt, arcs, ambigs, x);
 
   real_vector unw(npt);
-  for ( int i = 0; i < npt; ++i ) {
-      unw[i] += gauss[i] + 2*M_PI * x[i];
+  for (int i = 0; i < npt; ++i) {
+    unw[i] += gauss[i] + 2 * M_PI * x[i];
   }
 
-  fwrite(&size,sizeof(int),1,::stdout);
-  fwrite(&unw[0],sizeof(double),unw.size(),::stdout);
-
+  fwrite(&size, sizeof(int), 1, ::stdout);
+  fwrite(&unw[0], sizeof(double), unw.size(), ::stdout);
 }
--- src/maxflow_undirected_slimcuts.h
+++ src/maxflow_undirected_slimcuts.h
@@ -33,6 +33,8 @@
 #include <unordered_map>
 #include <vector>
 
+#include <cassert>
+
 namespace maxflowlib {
 
 template <typename GraphMaxflow>
@@ -45,16 +47,7 @@ public:
   typedef typename GraphMaxflow::arcid arcid;
   typedef typename GraphMaxflow::cap cap;
   typedef typename GraphMaxflow::flow flow;
-
-  void add_to_adj_map(nodeid s, nodeid t, cap c) {
-    adjacency_map &map = m_adj[s];
-    adjacency_map::iterator it = map.find(t);
-    if (it != map.end()) { // found, increase
-      it->second = c;
-    } else { // not found, create
-      map.insert(std::make_pair(t, c));
-    }
-  }
+  typedef UndirectedGraph<nodeid, arcid, cap, flow> BaseGraph;
 
 private:
   typedef std::unordered_map<nodeid, flow> adjacency_map;
@@ -63,11 +56,235 @@ private:
   const nodeid SOURCEID, SINKID;
 
   adjacency_list m_adj;
+  std::vector<flow> m_total_cap_at_node;
+  std::vector<nodeid> m_super_node;
+  std::vector<nodeid> m_new_super_node_id;
+  std::vector<bool> m_what_segment;
 
+  void add_to_adj_map(nodeid s, nodeid t, cap c) {
+    adjacency_map &map = m_adj[s];
+    typename adjacency_map::iterator it = map.find(t);
+    if (it != map.end()) { // found, increase
+      it->second = c;
+    } else { // not found, create
+      map.insert(std::make_pair(t, c));
+    }
+  }
+
+  void init_graph_contraction() {
+    m_total_cap_at_node.resize(BaseGraph::m_nnode);
+    for (nodeid i = 0; i < BaseGraph::m_nnode; ++i) {
+      adjacency_map &lmap = m_adj[i];
+      flow total = 0;
+      for (auto &it : lmap) {
+        total += it.second;
+      }
+      m_total_cap_at_node[i] = total;
+    }
+    m_super_node.assign(BaseGraph::m_nnode, 0);
+    for (nodeid i = 0; i < BaseGraph::m_nnode; ++i) {
+      m_super_node[i] = i;
+    }
+  }
+
+  void contract_edge(nodeid u, nodeid v) {
+    // contract u into v
+    m_super_node[u] = v;
+    adjacency_map &umap = m_adj[u];
+    adjacency_map &vmap = m_adj[v];
+    for (auto &it : umap) {
+      nodeid t = it.first;
+      if (t == v)
+        continue;
+      //      fprintf(stderr, "curr: %d, rewiring (%d,%d) ->
+      //      (%d,%d)\n",u,u,v,t,v);
+      flow f = it.second;
+      m_adj[t][v] += f;
+      m_adj[v][t] += f;
+      m_total_cap_at_node[v] += f;
+      m_adj[t].erase(u); // get rid of any trace of u
+    }
+    // u no longer exists
+    vmap.erase(u);
+    m_total_cap_at_node[u] = 0;
+    m_total_cap_at_node[v] -= umap[v];
+    umap.clear();
+    //    fprintf(stderr," clearing map of %d\n", u);
+  }
+
+  bool contract_node(nodeid u) {
+    flow total_cap = m_total_cap_at_node[u];
+    adjacency_map &umap = m_adj[u];
+    for (auto &it : umap) {
+      nodeid v = it.first;
+      flow f = it.second;
+      if (f > total_cap - f) {
+        contract_edge(std::min(u, v), std::max(u, v));
+        //        fprintf(stderr,"contracting edge (%d,%d)\n",std::min(u, v),
+        //        std::max(u, v));
+        return true;
+      }
+    }
+    return false;
+  }
+
+  void contract_graph() {
+    init_graph_contraction();
+    nodeid num_contracted = 0;
+    bool change;
+    do {
+      change = false;
+      for (nodeid id = 0; id < BaseGraph::m_nnode - 2; ++id) {
+        if (m_super_node[id] == id) { // is a supernode
+          bool success = contract_node(id);
+          change |= success;
+          if (success) {
+            num_contracted++;
+          }
+        }
+      }
+    } while (change);
+
+    fprintf(stderr, "number contracted: %d\n", num_contracted);
+    nodeid num_supernode = 0;
+    for (nodeid id = 0; id < BaseGraph::m_nnode; ++id) {
+      if (m_super_node[id] == id) { // is a supernode
+        num_supernode++;
+      }
+    }
+
+    fprintf(stderr, "number supernode: %d\n", num_supernode);
+    fprintf(stderr, "number original: %d\n", BaseGraph::m_nnode);
+  }
+
+  nodeid count_supernodes() const {
+    nodeid num_supernode = 0;
+    for (nodeid id = 0; id < BaseGraph::m_nnode; ++id) {
+      if (m_super_node[id] == id) { // is a supernode
+        num_supernode++;
+      }
+    }
+    return num_supernode;
+  }
+
+  arcid count_arcs() const {
+    arcid num_arc = 0;
+    for (nodeid u = 0; u < BaseGraph::m_nnode - 2; ++u) {
+      const adjacency_map &umap = m_adj[u];
+      for (auto &it : umap) {
+        nodeid v = it.first;
+        if (u < v) {
+          num_arc++;
+        }
+      }
+    }
+    return num_arc;
+  }
+
+  void resolve_super_nodes() {
+    for (nodeid u = 0; u < BaseGraph::m_nnode - 2; ++u) {
+      nodeid v = m_super_node[u];
+      while (v != m_super_node[v]) {
+        v = m_super_node[v];
+      }
+      m_super_node[u] = v;
+    }
+  }
+
+  void set_new_super_node_id() {
+    m_new_super_node_id.assign(BaseGraph::m_nnode, -1);
+    nodeid super_node_count = 0;
+    for (nodeid u = 0; u < BaseGraph::m_nnode - 2; ++u) {
+      if (m_super_node[u] == u) { // is a supernode
+        m_new_super_node_id[u] = super_node_count++;
+      } else {
+        if (m_adj[u].size() > 0) {
+          //      fprintf(stderr, "non-supernode with edges: %d\n", u);
+          assert(false);
+        }
+      }
+    }
+  }
+
+  void simplify_st_arcs() {
+    adjacency_map &smap = m_adj[SOURCEID];
+    smap.erase(SINKID);
+    adjacency_map &tmap = m_adj[SINKID];
+    tmap.erase(SOURCEID);
+    for (nodeid u = 0; u < BaseGraph::m_nnode - 2; ++u) {
+      adjacency_map &umap = m_adj[u];
+      if (umap.find(SOURCEID) != umap.end() &&
+          umap.find(SINKID) != umap.end()) {
+        flow &sflow = umap[SOURCEID];
+        flow &tflow = umap[SINKID];
+        if (sflow > tflow) { // source wins
+          sflow -= tflow;
+          smap[u] -= tflow;
+          umap.erase(SINKID);
+          tmap.erase(u);
+        } else if (sflow < tflow) { // sink wins
+          tflow -= sflow;
+          tmap[u] -= sflow;
+          umap.erase(SOURCEID);
+          smap.erase(u);
+        } else { // erase everthing
+          umap.erase(SOURCEID);
+          smap.erase(u);
+          umap.erase(SINKID);
+          tmap.erase(u);
+        }
+      }
+    }
+  }
+
+  void add_arcs(GraphMaxflow &g) const {
+    for (nodeid u = 0; u < BaseGraph::m_nnode - 2; ++u) {
+      const adjacency_map &umap = m_adj[u];
+      for (auto &it : umap) {
+        nodeid v = it.first;
+        flow f = it.second;
+        if (u < v && (v < BaseGraph::m_nnode - 2)) {
+          g.add_arc(m_new_super_node_id[u], m_new_super_node_id[v], f, f);
+        }
+      }
+    }
+    { // add source capacities
+      const adjacency_map &smap = m_adj[SOURCEID];
+      for (auto &it : smap) {
+        nodeid v = it.first;
+        flow f = it.second;
+        g.set_tweights(m_new_super_node_id[v], f, 0);
+      }
+    }
+    { // add sink capacities
+      const adjacency_map &smap = m_adj[SINKID];
+      for (auto &it : smap) {
+        nodeid v = it.first;
+        flow f = it.second;
+        g.set_tweights(m_new_super_node_id[v], 0, f);
+      }
+    }
+  }
+
+  void get_what_segments(GraphMaxflow &g) {
+    m_what_segment.assign(BaseGraph::m_nnode - 2, false);
+    for (nodeid u = 0; u < BaseGraph::m_nnode - 2; ++u) {
+      nodeid sn = m_super_node[u];
+      if (sn == SINKID) {
+        m_what_segment[u] = true;
+      } else if (sn == SOURCEID) {
+        m_what_segment[u] = false;
+      } else {
+        nodeid nu = m_new_super_node_id[sn];
+        m_what_segment[u] = g.what_segment(nu);
+      }
+    }
+  }
+
+public:
   UndirectedGraphSlimCuts(nodeid nnode)
-      : UndirectedGraph(nnode + 2), m_adj(UndirectedGraph::m_nnode),
-        SOURCEID(UndirectedGraph::m_nnode - 2),
-        SINKID(UndirectedGraph::m_nnode - 1) {}
+      : BaseGraph(nnode + 2), m_adj(BaseGraph::m_nnode),
+        SOURCEID(BaseGraph::m_nnode - 2), SINKID(BaseGraph::m_nnode - 1) {}
 
   void add_arc(nodeid s, nodeid t, cap c) {
     if (c > 0) {
@@ -86,6 +303,37 @@ private:
       add_to_adj_map(SINKID, s, tcap);
     }
   }
+
+  //  void test_contraction() { contract_graph(); }
+
+  flow maxflow() {
+
+    contract_graph();
+    resolve_super_nodes();
+
+    nodeid num_node = count_supernodes() - 2; // don't count source/sink
+    if (!num_node) {
+      m_what_segment.assign(BaseGraph::m_nnode - 2, false);
+      return 0;
+    }
+    arcid num_arc = count_arcs();
+
+
+    // sets new ids to use that are collapsed to contigious in a range
+    set_new_super_node_id();
+
+    simplify_st_arcs();
+
+    GraphMaxflow graph(num_node, num_arc);
+    add_arcs(graph);
+    flow f = graph.maxflow();
+
+    get_what_segments(graph);
+
+    return f;
+  }
+
+  bool what_segment(nodeid s) { return m_what_segment[s]; }
 };
 
 } // namespace maxflowlib
